e32fb72de82b891ccddbc069ce037a8568102f06
Kim Minseo <kimms010625@gmail.com>
2025-07-01 12:31:43 +0800

    SKR utils

---
+++ src/skr-utils.c
@@ -0,0 +1,8 @@
+/*
+ * This file contains the necessary utility functions for
+ * calculating the SKR for different links.
+ * This file should be included wherever SKR calculation
+ * is required. 
+*/
+
+#include "skr-utils.h"
---
+++ src/skr-utils.h
@@ -0,0 +1,5 @@
+#ifndef __SKR_UTILS_H__
+#define __SKR_UTILS_H__
+
+
+#endif
