8cf2296630c48f5751f7c00478351c3a3ee40880
AlexandreFontana <alexandrefontana02@gmail.com>
2025-12-02 13:09:01 +0800

    small fix. Add back commented out code.

--- src/gtk-multiple-sat.c
+++ src/gtk-multiple-sat.c
@@ -808,14 +808,9 @@ void gtk_multiple_sat_update(GtkWidget * widget, guint index)
                 //SAT(g_slist_nth_data(msat->sats, msat->selected[index]));
                 SAT(g_slist_nth_data(msat->sats, g_array_index(msat->selected, guint, index)));
  
-            //TEMP FIX: when g_array is implemented for gtk-multiple-sat then
-            //should remove if statement
-            if (index < msat->dyn_num_sat) {
-                Calculate_RADec(sat, msat->qth, &astro);
-                sat->ra = Degrees(astro.ra);
-                sat->dec = Degrees(astro.dec);
- 
-            }
+            Calculate_RADec(sat, msat->qth, &astro);
+            sat->ra = Degrees(astro.ra);
+            sat->dec = Degrees(astro.dec);
         }
 
         // Update visible fields one by one
